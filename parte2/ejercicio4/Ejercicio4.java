0d1a0e44ba85acefba42d370f9cf2bab6cbbd9e4
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-04-10 16:59:14 +0200

    Ejercicio4 de repaso de objetos

---
+++ parte2/ejercicio4/Ejercicio4.java
@@ -0,0 +1,12 @@
+package parte2.ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Diga una palabra");
+        String palabra = sc.nextLine();
+        System.out.printf("Hola %s",palabra);
+    }
+}
