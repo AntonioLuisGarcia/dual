9bff3c8e3601961e8c05ea323a6c063f72655ae1
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-04-11 13:07:05 +0200

    Ejercicio7 de repaso de objetos

---
+++ parte2/ejercicio7/Ejercicio7.java
@@ -0,0 +1,8 @@
+package parte2.ejercicio7;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Etiqueta e = new Etiqueta("p","Hola Mundo!!!");
+        System.out.println(e);
+    }
+}
